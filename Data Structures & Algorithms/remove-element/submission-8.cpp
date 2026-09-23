043ea45b3f6f29ae765065fbb543738466a95053
Likhita Jonnakuti <94250445+LJ0704@users.noreply.github.com>
2026-09-23 10:39:06 -0600

    Add: remove-element - submission-8

---
+++ Data Structures & Algorithms/remove-element/submission-8.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        //Brute Force Method: 
+        // int j = 0;
+        // int k = 0;
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     if(nums[i] == val)
+        //     {
+        //         k++;
+        //         continue;
+        //     }
+
+        //     if(i != j)
+        //     {
+        //         nums[j] = nums[i];
+        //     }
+        //     j++; 
+        // }
+        // return j;
+
+        //Two Pointer Method
+        int i = 0; 
+   
+        int n = nums.size();
+        
+        int k = 0;
+         while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+         }
+        return n;
+    }
+
+};
