bc6fc4ca85477c7fbf2939c633b1995a85840b53
Likhita Jonnakuti <94250445+LJ0704@users.noreply.github.com>
2026-09-24 12:20:03 -0600

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        //Can convert string to binary string?
+        string encoded_string = "";
+        for(auto& str : strs)
+        {
+            for(auto& c : str)
+            {
+                encoded_string.push_back(c);
+            }
+            encoded_string.push_back((char)0xFFFFFF);
+        }
+        return encoded_string;
+    }
+
+    vector<string> decode(string s) {
+
+        vector<string> decoded_strs;
+
+        string str = "";
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == (char)0xFFFFFF)
+            {
+                decoded_strs.push_back(str);
+                str = "";
+            }
+            else
+            {
+                str.push_back(s[i]);
+            }
+        }
+        return decoded_strs;
+    }
+};
